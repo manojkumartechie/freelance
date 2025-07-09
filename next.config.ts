a9403fe66eb3b22154d572cbf39d587386671dde
Manoj Kumar K <manojkumar9384@outlook.com>
2025-07-09 12:39:14 +0530

    Fix Module Build Error by Adding Transpile Packages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  transpilePackages: ['@react-three/drei', '@react-three/fiber', 'three'],
 };
 
 export default nextConfig;
